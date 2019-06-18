03664c9a1d1112d0335bb33dc15418f1b92fcdc8
jakuborlinski <jakub@elnino.tech>
2019-06-18 10:55:24 +0200

    initial parsers

--- Frontend.hs
+++ Frontend.hs
@@ -7,3 +7,56 @@ import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
 
+import Grammar
+
+
+
+-- Programs
+parseProgram = Program <$> (many parseDefinition)
+parseDefinition = FunctionDef <$> parseFunctionDef
+               <|> VariableDef <$> parseVariableDef <* semi
+               <|> GlobalDef <$> parseGlobalDef <* semi
+
+-- Variables
+parseVariableDef = Variable <$> parseType <*> identifier <*> parseExpression
+parseGlobalDef = Global <$> (commaSep identifier)
+parseType = (IntType <$> reserved "int") <|> (BoolType <$> reserved "boolean") <|> (VoidType <$> reserved "void")
+
+-- Functions
+parseParam = Param <$> parseType <*> identifier
+parseFunctionDef = Function <$> parseType <*> identifier <*> parens (commaSep parseParam) <*> (many parseStatement)
+
+-- Language
+parseStatement = SmtDef <$> parseDefinition
+              <|> SmtIf <$> reserved "if" *> (parens parseExpression) <*> (braces (many parseStatement)) <* reserved "else" <*> (braces (many parseStatement))
+              <|> SmtWhile <$> reserved "while" *> (parens parseExpression) <*> (braces (many parseStatement))
+              <|> SmtRet <$> parseExpression
+              <|> SmtAss <$> identifier <* symbol "=" <*> parseExpression
+              <|> SmtCall <$> identifier <*> (parens (commaSep parseExpression))
+              <|> SmtExpr <$> parseExpression
+
+
+
+-- Algebra
+add' = (\_ -> (ExprAdd)) <$> symbol "+"
+
+sub' = (\_ -> (ExprSubtract)) <$> symbol "-"
+
+mult' = (\_ -> (ExprMult)) <$> symbol "*"
+
+parseExpression = parseTerm `chainr` add' <|> parseTerm `chainr1` sub'
+
+parseTerm = parseFact `chainr` mult'
+
+parseFact = (ExprConst <$> integer)
+            <|> try (ExprCall <$> identifier <*> (parens (commaSep parseExpression)))
+            <|> (ExprVar <$> identifier)
+            <|> (ExprBrac <$> (parens parseExpression))
+            <|> (ExprBool <$> parseExpression <*> parseOrder <*> parseExpression)
+
+parseOrder = OrderLT <$> reserved "<"
+          <|> OrderLE <$> reserved "<="
+          <|> OrderEQ <$> reserved "=="
+          <|> OrderNE <$> reserved "!="
+          <|> OrderGT <$> reserved ">"
+          <|> OrderGE <$> reserved ">="
--- Grammar.hs
+++ Grammar.hs
@@ -2,8 +2,167 @@ module Grammar where
 
 import Data.List
 import Data.Char
+import Data.Functor
+import Data.Either
 
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as Token
+
+
+{-
+
+program : definition *;
+
+definition : functionDef
+           | variableDef SEMI
+           | globalDef SEMI;
+
+// Variables
+variableDef : type identifier ASS expression;
+
+array : LSQBRACKET (item ',') * item RSQBRACKET;
+
+type: BOOLTYPE | INTTYPE | ARRTYPE;
+
+globalDef: GLOBAL (identifier ',')* identifier;
+
+// Functions
+functionDef : FUNCTION retType identifier LPAR params RPAR LBRACK statement+ RBRACK;
+
+retType: type | VOID;
+
+params: ((type identifier ',')* type identifier)*;
+
+
+// Language
+statement : defintion
+          | if
+          | while
+          | RETURN identifier
+          | expression;
+
+if: IF LPAR bool RPAR LBRACK statement * RBRACK (ELSE LBRACK statement * RBRACK )*;
+
+while: WHILE LPAR bool RPAR LBRACK statement * RBRACK;
+
+bool: expression ordering expression | identifier;
+
+ordering: LT | LE | EQ | NE | GT | GE;
+
+
+expression : term
+     | term ( '+' | '-' ) expression;
+
+term : factor
+     | factor '*' term;
+
+factor : INTEGER | BOOLEAN | array
+       | identifier LPAR ((arg ',')* arg) * RPAR
+       | identifier
+       | '(' expr ')';
+
+-}
+
+data Program = Program [Definition]
+                deriving (Show)
+
+data Definition = FunctionDef
+                | VariableDef
+                | GlobalDef
+                deriving (Show)
+
+-- Variables
+data VariableDef = Variable Type String Expression
+                deriving (Show)
+
+data Type = IntType () | BoolType () | VoidType ()
+                deriving (Show)
+
+data GlobalDef = Global [String]
+                deriving (Show)
+
+
+-- Functions
+data FunctionDef = Function RetType String [Param] [Statement]
+                deriving (Show)
+
+data Param = Param Type String
+                deriving (Show)
+
+data RetType = RetType Type
+                deriving (Show)
+
+
+-- Language
+data Statement = SmtDef Definition
+               | SmtIf Expression [Statement] [Statement]
+               | SmtWhile Expression [Statement]
+               | SmtRet Expression
+               | SmtAss String Expression
+               | SmtCall String [Expression]
+                deriving (Show)
+
+
+data Order = OrderLT () | OrderLE () | OrderEQ () | OrderNE () | OrderGT () | OrderGE ()
+                deriving (Show)
+
+data Expression = ExprConst Integer
+          | ExprVar String
+          | ExprAdd Expression Expression
+          | ExprSubtract Expression Expression
+          | ExprMult Expression Expression
+          | ExprBrac Expression
+          | ExprBool Expression Order Expression
+          | ExprCall String [Expression]
+          deriving Show
+
+-- data Expression =
+--                   ExprTerm Term
+--                 | ExprPlus Term Expression
+--                 | ExprSub Term Expression
+--                 | ExprBool Order Term Expression
+--                 deriving (Show)
+--
+-- data Term = TermFact Factor
+--           | TermMult Factor Term
+--                 deriving (Show)
+--
+-- data Factor = FactorInt Integer
+--             | FactorTrue ()
+--             | FactorFalse ()
+--             | FactorCall String [Expression]
+--             | FactorId String
+--             | FactorBrack Expression
+--                 deriving (Show)
+
+fromLeft' :: Either l r -> l
+fromLeft' (Left x) = x -- Newer GHC versions contain a fromLeft :: l -> Either l r -> l
+
+fromRight' :: Either l r -> r
+fromRight' (Right x) = x -- Newer GHC versions contain a fromRight :: r -> Either l r -> r
+
+parser :: Parser a -> String -> a
+parser p xs | isLeft res = error $ show $ fromLeft' res
+          | otherwise  = fromRight' res
+  where res = parse p "" xs
+
+languageDef = emptyDef {
+                Token.reservedOpNames = ["=", "+", "*", "-", "==", "!=", "<", "<=", ">", ">="],
+                Token.reservedNames = ["if", "while", "else", "function", "return", "int", "boolean", "true", "false", ","],
+                Token.identStart = letter,
+                Token.identLetter = alphaNum,
+                Token.commentLine = "//"
+              }
+
+lexer = Token.makeTokenParser languageDef
+
+symbol = Token.symbol lexer
+parens = Token.parens lexer
+braces = Token.braces lexer
+identifier = Token.identifier lexer
+integer = Token.integer lexer
+reserved = Token.reserved lexer
+commaSep = Token.commaSep lexer
+
 
